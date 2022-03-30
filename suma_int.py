271af5a8b7290770f90065c1358674b44de50b71
AuraSeg <ada_hack131@hackademy.mx>
2022-03-29 23:44:23 -0600

    Ejercicio suma enteros

---
+++ suma_int.py
@@ -0,0 +1,10 @@
+
+
+def suma(a, b):
+    return a+b
+
+num1 = float(input("Escriba el primer número: "))
+num2 = float(input("Escriba el segundo número: "))
+res = suma(num1, num2)
+print(res)
+print(f"El resultado de la suma es: {res}")
